fd1a7fbaa42e5b44441aa3735616f979256855dd
HYNCM <cmhyn1015@gmail.com>
2022-09-22 18:20:24 +0800

    add new scripts

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
-  <div>
-    <h2>provide和inject</h2>
-    <p>当前的颜色:{{ color }}</p>
-    <button @click="color = 'red'">红色</button>
-    <button @click="color = 'green'">绿色</button>
-    <button @click="color = 'blue'">蓝色</button>
-    <hr />
-    <Son />
-  </div>
+  <div></div>
 </template>
 <script lang="ts">
-import { defineComponent, ref, provide } from "vue";
-import Son from "./components/Son.vue";
+import { defineComponent } from "vue";
 export default defineComponent({
   name: "App",
-  components: {
-    Son,
-  },
   setup() {
-    //响应式数据
-    const color = ref("red");
-    //提供数据
-    provide("color", color);
-    return {
-      color,
-    };
+    return {};
   },
 });
 </script>
